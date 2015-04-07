757d00dee11ceee9cc531e0418feda69a434a9a4
Winnu Ayi Satria <winnuayi@gmail.com>
2015-04-07 14:46:26 +0700

    simple mongoose

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/test');
+
+var api = require('./routes/api');
 
 var app = express();
+var router = express.Router();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -22,8 +25,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/api', api);
+
+//app.use(function(req, res, next) {
+//  console.log('Something is happening');
+//  next();
+//});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

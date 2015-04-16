b0c5ac831f24a0cf386bd2513dfbdfb182e3096b
Winnu Ayi Satria <winnuayi@gmail.com>
2015-04-16 17:09:33 +0700

    add CORS handler

--- app.js
+++ app.js
@@ -27,6 +27,16 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', api);
 
+var allowCrossDomain = function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+  next();
+};
+
+app.use(allowCrossDomain);
+
 //app.use(function(req, res, next) {
 //  console.log('Something is happening');
 //  next();
